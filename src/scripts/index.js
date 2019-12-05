8216c0ff34c1199886832cfc84111a7182cd3997
Radek <radek511@op.pl>
2019-12-05 22:26:16 +0000

    Basic structure of scss & js files with API request

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,20 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+const API_KEY = '7c511c48c59d892d7a703f33e465470c3958fbbbc57a5f32c5cf1320f4f72e11';
+
+const applicationInit = () => {
+  fetchImagesData();
+};
+
+const fetchImagesData = async () => {
+  try {
+    const response = await fetch(`https://api.unsplash.com/photos/?client_id=${API_KEY}`);
+    const fetchedData = await response.json();
+
+    console.log(fetchedData);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+applicationInit();
